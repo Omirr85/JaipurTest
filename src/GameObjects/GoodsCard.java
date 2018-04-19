f3acdc086f1ae2072582f5c88f17242163e14c6c
Omirr85 <david.vlaminck@telenet.be>
2018-04-19 23:52:13 +0200

    1

--- src/GameObjects/GoodsCard.java
+++ src/GameObjects/GoodsCard.java
@@ -1,6 +1,10 @@
 package GameObjects;
 
 public class GoodsCard extends Card {
+    public GoodsCard(GameObjects.GoodType goodtype) {
+        setGoodType(goodtype);
+    }
+
     public GoodType getGoodType() {
         return GoodType;
     }
